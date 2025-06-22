23c4dc1d834e1c1f8cf04e192b02b77e0c32b3cc
HooeyMagooes <macdwg2407@gmail.com>
2025-06-23 08:54:48 +1000

    Added csv reader to take the information from the file and add it to the person class

--- Group Preferential Sorter/Form1.vb
+++ Group Preferential Sorter/Form1.vb
@@ -1,7 +1,8 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
 
     Public Function PreferentialSort(people As List(Of Person), groups As Dictionary(Of String, List(Of Person)), maxSize As Integer) As Dictionary(Of String, List(Of Person))
-
         For Each person As Person In people
             If Not String.IsNullOrEmpty(person.Inclusion) Then
                 Dim groupId As String = person.Inclusion
@@ -50,10 +51,43 @@
         Return groups
     End Function
 
+    Public Function LoadPeopleFromCsv(filePath As String) As List(Of Person)
+        Dim people As New List(Of Person)()
+
+        For Each line As String In File.ReadLines(filePath)
+            If String.IsNullOrWhiteSpace(line) Then Continue For
+            Dim fields = line.Split(","c)
+
+            If fields.Length < 6 Then Continue For
+
+            Dim person As New Person()
+            person.Name = fields(0).Trim()
+
+            For i As Integer = 1 To 5
+                If fields.Length > i AndAlso Not String.IsNullOrWhiteSpace(fields(i)) Then
+                    person.Preferences.Add(fields(i).Trim())
+                End If
+            Next
+
+            If fields.Length > 6 AndAlso Not String.IsNullOrWhiteSpace(fields(6)) Then
+                person.Inclusion = fields(6).Trim()
+            End If
+
+            If fields.Length > 7 AndAlso Not String.IsNullOrWhiteSpace(fields(7)) Then
+                person.Exclusion = fields(7).Trim()
+            End If
+
+            people.Add(person)
+        Next
+
+        Return people
+    End Function
+
 End Class
 
 Public Class Person
 
+    Public Name As String
     Public Preferences As List(Of String)
     Public Inclusion As String
     Public Exclusion As String
@@ -62,6 +96,7 @@ Public Class Person
 
     Public Sub New()
         Preferences = New List(Of String)()
+        ExcludedGroups = New HashSet(Of String)()
         AssignedGroup = Nothing
     End Sub
 End Class
