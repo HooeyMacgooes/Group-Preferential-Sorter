07701bd77bdbfdb4a5db006cf2cc25d86f9db212
HooeyMagooes <macdwg2407@gmail.com>
2025-06-23 08:10:11 +1000

    First sorting algorithm attempt no other functionality

--- Group Preferential Sorter/Form1.Designer.vb
+++ Group Preferential Sorter/Form1.Designer.vb
@@ -22,10 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1128, 558)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- Group Preferential Sorter/Form1.vb
+++ Group Preferential Sorter/Form1.vb
@@ -1,3 +1,70 @@
 ﻿Public Class Form1
 
+    Public Function PreferentialSort(people As List(Of Person), groups As Dictionary(Of String, List(Of Person)), maxSize As Integer) As Dictionary(Of String, List(Of Person))
+
+        For Each person As Person In people
+            If Not String.IsNullOrEmpty(person.Inclusion) Then
+                Dim groupId As String = person.Inclusion
+                If groups.ContainsKey(groupId) AndAlso groups(groupId).Count < maxSize Then
+                    groups(groupId).Add(person)
+                    person.AssignedGroup = groupId
+                Else
+
+                End If
+            End If
+        Next
+
+        For Each person As Person In people
+            If Not String.IsNullOrEmpty(person.Exclusion) Then
+                person.ExcludedGroups.Add(person.Exclusion)
+            End If
+        Next
+
+        For Each person As Person In people
+            If String.IsNullOrEmpty(person.AssignedGroup) Then
+                For Each Preference As String In person.Preferences
+                    If Not person.ExcludedGroups.Contains(Preference) AndAlso groups.ContainsKey(Preference) AndAlso groups(Preference).Count < maxSize Then
+                        groups(Preference).Add(person)
+                        person.AssignedGroup = Preference
+                        Exit For
+                    End If
+                Next
+            End If
+        Next
+
+        For Each person As Person In people
+            If String.IsNullOrEmpty(person.AssignedGroup) Then
+                For Each kvp As KeyValuePair(Of String, List(Of Person)) In groups
+                    Dim groupId As String = kvp.Key
+                    Dim memberList As List(Of Person) = kvp.Value
+
+                    If memberList.Count < maxSize AndAlso Not person.ExcludedGroups.Contains(groupId) Then
+                        memberList.Add(person)
+                        person.AssignedGroup = groupId
+                        Exit For
+                    End If
+                Next
+            End If
+        Next
+
+        Return groups
+    End Function
+
 End Class
+
+Public Class Person
+
+    Public Preferences As List(Of String)
+    Public Inclusion As String
+    Public Exclusion As String
+    Public AssignedGroup As String
+    Public ExcludedGroups As HashSet(Of String)
+
+    Public Sub New()
+        Preferences = New List(Of String)()
+        AssignedGroup = Nothing
+    End Sub
+End Class
+
+
+
